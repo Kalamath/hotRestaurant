49015c5591d4fe05a7f08c1dff4f17fb58d334ba
Luis Villalobos <vlobos323@gmail.com>
2019-09-07 11:37:14 -0700

    added routes for reservations

--- app.js
+++ app.js
@@ -7,8 +7,20 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+let reservations = [];
 
-
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+  });
+  
+  app.get("/add", function(req, res) {
+    res.sendFile(path.join(__dirname, "add.html"));
+  });
+  
+  // Displays all reservations
+  app.get("/api/reservations", function(req, res) {
+    return res.json(reservations);
+  });
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
